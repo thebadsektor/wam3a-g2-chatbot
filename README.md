62d6e87edd3d597c072d7d4afd109881ed609027
Trovela, Angelo Bergado <1116angelotrovela@gmail.com>
2023-05-31 06:59:32 +0800

    group 2 commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## STEPS
+1. Create a virtual environment ``` python -m venv .venv ```
+2. Activate the .venv ``` source .venv/Scripts/activate ```
+3. Install flask ``` pip install Flask ```
+4. Install pandas to read the .CSV file ``` pip install flask pandas ```
+5. To retrieve the most likely match from the .CSV file install this ``` pip install fuzzywuzzy ```
--- app.py
+++ app.py
@@ -1,42 +1,44 @@
-#! /usr/bin/python3
 from flask import Flask, render_template, request
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 import random
-import csv
-import os
-from botRespond import getResponse
+
+
 
 app = Flask(__name__)
 
-chatbotName = 'Gerabot'
+# Load the CSV data into a pandas DataFrame
+data = pd.read_csv('chatbot_data.csv', on_bad_lines='skip')
 
-#Create Log file
-try:
-    file = open('data/log.csv', 'r')
-except IOError:
-    file = open('data/log.csv', 'w')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_input = request.form['user_input']
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    botReply = str(getResponse(userText))
-    if botReply == "IDKresponse":
-        botReply = str(getResponse('IDKnull')) ##Send the i don't know code back to the DB
-    elif botReply == "getTIME":
-        botReply = "getTime()"
-    elif botReply == "getDATE":
-        botReply = "getDate()"
-    ##Log to CSV file
-    print("Logging to CSV file now")
-    with open('data/log.csv', 'a', newline='') as logFile:
-        newFileWriter = csv.writer(logFile)
-        newFileWriter.writerow([userText, botReply])
-        logFile.close()
-    return botReply
+    # Find the matching question in the DataFrame
+    matching_question = process.extractOne(user_input, data['question'], scorer=fuzz.token_sort_ratio)
 
 
-if __name__ == "__main__":
-    app.run()
+    if matching_question and matching_question[1] >= 60:
+        # Get the corresponding answer for the most likely match
+        response = data.loc[matching_question[2], 'answer']
+
+    else:
+        random_responses = [
+            "I'm sorry, I don't have an answer for that.",
+            "I'm still learning and don't have information on that.",
+            "I'm afraid I can't help with that question.",
+            "That's a good question. Unfortunately, I don't have the answer.",
+        ]
+        response = random.choice(random_responses)
+
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
